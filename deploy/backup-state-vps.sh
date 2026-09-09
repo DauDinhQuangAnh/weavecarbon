51c16b99f7b107a7b095a1d9e13ed7ec17d4003c
Dau Dinh Quang Anh <20521059@gm.uit.edu.vn>
2026-09-09 20:04:38 +0700

    fix: exclude temporary schemas from backup counts

--- deploy/backup-state-vps.sh
+++ deploy/backup-state-vps.sh
@@ -78,6 +78,8 @@ BEGIN
     SELECT schemaname, tablename
     FROM pg_tables
     WHERE schemaname NOT IN ('pg_catalog', 'information_schema')
+      AND schemaname NOT LIKE 'pg_temp_%'
+      AND schemaname NOT LIKE 'pg_toast_temp_%'
     ORDER BY schemaname, tablename
   LOOP
     EXECUTE format('SELECT count(*) FROM %I.%I', item.schemaname, item.tablename)
